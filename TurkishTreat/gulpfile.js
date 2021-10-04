38e6dcfb8d167b7cd8cb4a019657c740abcfa10e
Cecilia F Carter <ceciliafcarter@gmail.com>
2021-10-04 15:16:16 -0400

    minor changes

---
+++ TurkishTreat/gulpfile.js
@@ -0,0 +1,26 @@
+﻿var gulp = require("gulp");
+var uglify = require("gulp-uglify");
+var concat = require("gulp-concat");
+var rename = require("gulp-rename");
+var cleanCss = require("gulp-clean-css");
+
+//minify JavaScript
+function minify() {
+    return gulp.src(["wwwroot/js/**/*.js"])
+        .pipe(uglify())
+        .pipe(concat("turkishtreat.min.js"))
+        .pipe(gulp.dest("wwwroot/dist/"));
+}
+
+//minify CSS
+function styles() {
+    return gulp.src(["wwwroot/css/**/*.css"])
+        .pipe(rename("turkishtreat.min.css"))
+        .pipe(cleanCss())
+        .pipe(gulp.dest("wwwroot/dist/"));
+}
+
+exports.minify = minify;
+exports.styles = styles;
+
+exports.default = gulp.parallel(minify, styles);
